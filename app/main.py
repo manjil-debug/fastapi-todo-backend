2bd0460f6ceedb71c256c833e02537a2172b934d
Manjil Sthapit <manjil@nepotech.com>
2025-10-19 19:40:01 +0545

    corse

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.routers import todo ,user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="FastAPI Docker Test")
+
+origins = [
+    "http://localhost:3000",   # React local dev
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # allow GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],
+)
 
 app.include_router(todo.router)
 app.include_router(auth.router)
